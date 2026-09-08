732ef9e3f5af9e3ab42df940b875e8867a4d8fca
CiberThanatos16 <georochi@gmail.com>
2026-09-08 09:47:27 -0600

    Usar variable de entorno para la URL del API

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+/*
+//Local host de desarrollo 
 const api = axios.create({
     baseURL: "http://localhost:3000/api"
+});
+*/
+
+//API para produccion en Render/ conexion al backend deployed en Render
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api"
 });
 
 api.interceptors.request.use((config) => {
